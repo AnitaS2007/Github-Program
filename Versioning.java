90b5697b959fed19e7eaee64aabb3965b69f5cef
AnitaS2007 <anitahylakar@gmail.com>
2026-04-03 12:21:13 +0530

    Versioning.java program

---
+++ Versioning.java
@@ -0,0 +1,24 @@
+public class Version1{
+  public static void main(String[] args) {
+    System.out.println("==== HISTORY OF GITHUB ====\n");
+
+    System.out.println("GitHub was founded in 2008.");
+    System.out.println("Founders: Tom Preston-Werner,Chris Wantrath, PJ Hyett, Scott Chacon.\n");
+
+    System.out.println("Key Milestones:");
+    System.out.println("2008 - GitHub launched");
+    System.out.println("2010 - Reached 1M repositories");
+    System.out.println("2012 - 10M repositories");
+    System.out.println("2018 - Acquired by Microsoft");
+    System.out.println("2020+ - Become largest code hosting platform\n");
+
+    System.out.println("Why GitHub is Important":);
+    System.out.println("Version control using Git");
+    System.out.println("Collaboration between developers");
+    System.out.println("Open-source contributions");
+    System.out.println("Project management tools\n");
+
+    System.out.println("====THANK YOU ====");
+  }
+}
+  
